8c964a8749f438ffc8b24cd9f7c8241b01c5dc81
Vitor Scheffer <vitorscheffer61@gmail.com>
2023-02-06 17:40:25 -0300

    Initial Commit

---
+++ SeeMovies/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SeeMovies
+//
+//  Created by José Vitor Scheffer Boff dos Santos on 06/02/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
