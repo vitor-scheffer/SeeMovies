196ec80a89c1ed909c2a181237ace3b01614903c
Vitor Scheffer <vitorscheffer61@gmail.com>
2023-02-09 14:01:29 -0300

    Add signInView+Preview
    Adicionando signInView+Preview.
    Adicionando navegação da loginView para signInView através de delegate.

---
+++ SeeMovies/Controllers/Login/SignIn/Screen/SignInView.swift
@@ -0,0 +1,70 @@
+//
+//  SignInView.swift
+//  SeeMovies
+//
+//  Created by José Vitor Scheffer Boff dos Santos on 09/02/23.
+//
+
+import UIKit
+
+class SignInView: UIView {
+    
+    override init(frame: CGRect) {
+        super.init(frame: .zero)
+        
+        setupView()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private lazy var bgSignIn: UIImageView = {
+        let view = UIImageView()
+        
+        view.translatesAutoresizingMaskIntoConstraints = false
+        view.image = UIImage(named: "bgLaunch")
+        return view
+    }()
+    
+    private lazy var logoView: PrimaryLogo = {
+        let view = PrimaryLogo(frame: frame)
+        
+        return view
+    }()
+    
+}
+
+extension SignInView: ViewCode {
+    func buildHierarchy() {
+        self.addSubview(bgSignIn)
+        self.addSubview(logoView)
+    }
+    
+    func setupConstraints() {
+        NSLayoutConstraint.activate([
+            
+            bgSignIn.topAnchor.constraint(equalTo: self.topAnchor),
+            bgSignIn.leadingAnchor.constraint(equalTo: self.leadingAnchor),
+            bgSignIn.trailingAnchor.constraint(equalTo: self.trailingAnchor),
+            bgSignIn.bottomAnchor.constraint(equalTo: self.bottomAnchor),
+            
+            logoView.topAnchor.constraint(equalTo: safeAreaLayoutGuide.topAnchor),
+            logoView.centerXAnchor.constraint(equalTo: centerXAnchor),
+        ])
+    }
+    
+    func applyAdditionalChanges() {
+        
+    }
+}
+
+#if DEBUG
+import SwiftUI
+
+struct SignInView_Provider: PreviewProvider {
+    static var previews: some View {
+        return SignInView().showPreview()
+    }
+}
+#endif
